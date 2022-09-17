120471f5764343350b9c5f407e19dcb991f6759c
KrzysztofRajmund <knadlonek@gmail.com>
2022-09-17 15:12:57 +0200

    feat: adjusted gallery and modal to new images

--- src/components/Gallery/ModalGallery.tsx
+++ src/components/Gallery/ModalGallery.tsx
@@ -9,13 +9,14 @@ const ModalGallery = ({ id, url }: Props) => {
   console.log("ID:", id);
   return (
     <section className="modal-container">
-      <Image
-        className="modal-image"
-        src={url}
-        alt={`modal-${id}`}
-        width={1000}
-        height={666}
-      />
+      <div className="modal-image">
+        <Image
+          src={url}
+          alt={`modal-${id}`}
+          layout='fill'
+          objectFit="contain"
+        />
+      </div>
       <img
         className="prev-icon"
         src="/static/assets/prev-icon-white.png"
