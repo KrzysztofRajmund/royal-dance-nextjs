79b5af9f90ceccbd8976fea5709be3c9a661191e
KrzysztofRajmund <knadlonek@gmail.com>
2022-10-01 14:37:16 +0200

    feat: added dynamic url and meta tags

--- src/components/Offers/OffersList/OfferCard/OfferCard.tsx
+++ src/components/Offers/OffersList/OfferCard/OfferCard.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
+import React, { useMemo } from 'react';
+import { stringFormatting } from '../../../utilities/helpers/formatting';
 import Meta from '../../../utilities/Meta';
 import styles from './OfferCard.module.scss';
 
@@ -11,26 +12,18 @@ interface IOffer {
     subtitle: string;
 }
 
-export const OfferCard = ({ id, imageUrl, category, subtitle }: IOffer) => {
+export const OfferCard = ({ imageUrl, category, subtitle, id }: IOffer) => {
+
     return (
         <>
-            <Meta
-                webUrl={`https://www.royaldanceszczecin.pl/${category}/${subtitle}`}
-                keywords={`${subtitle}, ${category}, Szkoła Tańca Szczecin, Latino Solo Szczecin, Zajęcia dla Pań Szczecin, Taniec Szczecin, Lekcje dla nowożeńców Szczecin, Pierwszy Taniec Szczecin`}
-                type='text'
-                title={`Royal Dance Szkoła Tańca-${subtitle}`}
-                imageUrl='https://www.royaldanceszczecin.pl/_ipx/w_1080,q_75/%2Fstatic%2Fgallery%2Fimg11.jpg?url=%2Fstatic%2Fgallery%2Fimg11.jpg&w=1080&q=75'
-                description={`${subtitle}, ${category}, Szkoła Tańca Szczecin, Latino Solo Szczecin, Zajęcia dla Pań Szczecin, Taniec Szczecin, Lekcje dla nowożeńców Szczecin, Pierwszy Taniec Szczecin`}
-                imageAlt='Szkoła Tańca Szczecin'
-            />
-            <Link href={`/${category}/${subtitle}`} passHref>
+            <Link href={`/${category}/${stringFormatting(subtitle, " ", "-")}`} passHref>
                 <div className={styles.card}>
                     <div className={styles.image}>
                         <Image
                             placeholder='blur'
                             blurDataURL={`/static/offers/${imageUrl}`}
                             src={`/static/offers/${imageUrl}`}
-                            alt={`image-${subtitle}`}
+                            alt={`image-${stringFormatting(subtitle, " ", "-")}`}
                             layout='fill'
                             objectFit='cover'
                         />
---
+++ src/components/utilities/helpers/formatting.ts
@@ -0,0 +1,14 @@
+export const stringFormatting = (
+  content: string,
+  current: string,
+  exchanger: string
+) => {
+  if (current === '-') {
+    const capitalizeFirstLetter = content.slice(0, 1).toUpperCase();
+
+    return `${capitalizeFirstLetter}${content
+      .slice(1, content.length)
+      ?.replaceAll(current, exchanger)}`;
+  }
+  return content?.replaceAll(current, exchanger);
+};
